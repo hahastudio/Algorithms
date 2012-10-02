d07e15ef33083c5dcca3518277d1ada10ee4455d
Alan haha <guyuhao088@gmail.com>
2012-10-02 10:42:44 +0800

    bitwise operation version
    delete temporary comments

--- basic_arithmetic.py
+++ basic_arithmetic.py
@@ -32,19 +32,11 @@ def multiply2(x, y):
     """requires two n-bit positive integers x & y,
     return their product.
     Using Gauss's idea in divide-and-conquer algorithm."""
-    #xB, yB = bin(x)[2:], bin(y)[2:]
-    #xN, yN = len(xB), len(yB)
     n = int(max(log(x or 1, 2), log(y or 1, 2))) + 1
-    """
-    if xN > yN:
-        yB = '0' * (xN - yN) + yB
-    if yN > xN:
-        xB = '0' * (yN - xN) + xB
-    """
     if n == 1:
         return x * y
-    xL, xR = x>>(n>>1), x & ((1<<(n>>1)) - 1)#int(xB[:-(n/2)] or '0', 2), int(xB[-(n/2):] or '0', 2)
-    yL, yR = y>>(n>>1), y & ((1<<(n>>1)) - 1)#int(yB[:-(n/2)] or '0', 2), int(yB[-(n/2):] or '0', 2)
+    xL, xR = x>>(n>>1), x & ((1<<(n>>1)) - 1)
+    yL, yR = y>>(n>>1), y & ((1<<(n>>1)) - 1)
     p1 = multiply2(xL, yL)
     p2 = multiply2(xR, yR)
     p3 = multiply2(xL+xR, yL+yR)
